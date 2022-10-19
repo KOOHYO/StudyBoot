901a17b57760c1f621ad93373c7ed9d32a57c5ff
KOOHYO <zpfhzpfh112@naver.com>
2022-10-19 14:11:04 +0900

    파일 업로드 하는중

--- src/main/java/com/ko/home/board/qna/QnaController.java
+++ src/main/java/com/ko/home/board/qna/QnaController.java
@@ -8,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ko.home.util.Pager;
 
@@ -21,6 +24,18 @@ public class QnaController {
 	
 	@Autowired
 	private QnaService qnaService;
+	
+	@PostMapping("add")
+	public String setAdd(QnaVO qnaVO, RedirectAttributes redirectAttributes)throws Exception {
+		int result = qnaService.setAdd(qnaVO);
+		redirectAttributes.addAttribute("result", result);
+		return "redirect:./list";
+	}
+	
+	@GetMapping("add")
+	public String setAdd()throws Exception{
+		return "board/write";
+	}
 	
 	@GetMapping("list")
 	public ModelAndView getList(Pager pager)throws Exception{
--- src/main/java/com/ko/home/board/qna/QnaService.java
+++ src/main/java/com/ko/home/board/qna/QnaService.java
@@ -4,10 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.ko.home.util.Pager;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+//로그객체를 생성하지않고 바로 로그를 찍을 수 있다.
+@Slf4j
 public class QnaService {
 
 	@Autowired
@@ -19,7 +24,12 @@ public class QnaService {
 	}
 	
 	public int setAdd(QnaVO qnaVO)throws Exception{
-		return qnaMapper.setAdd(qnaVO);
+		for(MultipartFile f : qnaVO.getFiles()) {
+			if(!f.isEmpty()) {
+				log.info("FileName {} ", f.getOriginalFilename());
+			}
+		}
+		return 1;//qnaMapper.setAdd(qnaVO);
 	}
 	
 }
