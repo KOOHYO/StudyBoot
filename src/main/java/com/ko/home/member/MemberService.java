01934053d4c39d9ecdd143c281d6538d8ee6f52a
KOOHYO <zpfhzpfh112@naver.com>
2022-10-31 09:47:56 +0900

    회원가입검증

--- src/main/java/com/ko/home/member/MemberService.java
+++ src/main/java/com/ko/home/member/MemberService.java
@@ -3,6 +3,7 @@ package com.ko.home.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.validation.BindingResult;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -14,14 +15,32 @@ public class MemberService {
 	@Autowired
 	private MemberMapper memberMapper;
 	
+	
+	// 사용자 정의 검증 메서드
+	public boolean getMemberError(MemberVO memberVO, BindingResult bindingResult)throws Exception{
+		boolean check=false;
+		// check=false : 검증성공(error 없음)
+		// check=true  : 검증실패(error 있음)
+		
+		// password가 일치하는지 검증
+		if(memberVO.getPw().equals(memberVO.getPwCheck())) {
+			check=true;
+		}
+		
+		return check;
+	}
+	
+	// 아이디 중복확인
 	public int getIdCheck(MemberVO memberVO)throws Exception{
 		return memberMapper.getIdCheck(memberVO);
 	}
 	
+	// 로그인
 	public MemberVO getLogin(MemberVO memberVO)throws Exception{
 		return memberMapper.getLogin(memberVO);
 	}
 	
+	// 회원가입
 	public int setJoin(MemberVO memberVO)throws Exception{
 		int result = memberMapper.setJoin(memberVO);
 		//강제로 Exception 발생시킴
