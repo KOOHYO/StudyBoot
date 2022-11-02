614f2a8a9f747f1b35ce85fa4aa8511f54070dc4
KOOHYO <zpfhzpfh112@naver.com>
2022-11-02 18:27:57 +0900

    시큐리티 적용!

--- src/main/java/com/ko/home/member/MemberVO.java
+++ src/main/java/com/ko/home/member/MemberVO.java
@@ -1,9 +1,11 @@
 package com.ko.home.member;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import java.sql.Date;
 import java.util.List;
@@ -43,8 +45,15 @@ public class MemberVO implements UserDetails {
 	private List<MemberRoleVO> memberRoleVOs;
 
 	@Override
+    // ?는 any 를 뜻함, extends GrantedAuthority 를 상속받는 아무타입이면 된다.
+    // <? super T> T나 T의 부모타입을 허용하겠다 라는 뜻
 	public Collection<? extends GrantedAuthority> getAuthorities() {
-		// TODO Auto-generated method stub
+		List<GrantedAuthority> authorities = new ArrayList<>();
+		for(MemberRoleVO memberRoleVO : memberRoleVOs) {
+			// SimpleGrantedAuthority 객체 생성 후 안에 String 타입의 Role을 넣어줌
+			// authorities.add List에 데이터를 추가
+			authorities.add(new SimpleGrantedAuthority(memberRoleVO.getRoleName()));
+		}
 		return null;
 	}
 
@@ -62,22 +71,31 @@ public class MemberVO implements UserDetails {
 
 	@Override
 	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		// 계정의 만료 여부
+		// true  : 만료 안됨
+		// false : 만료 됨, 로그인 불가
+		return true;
 	}
 
 	@Override
 	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+		// 계정 잠김 여부
+		// true  : 계정이 잠기지 않음
+		// false : 계정이 잠김, 로그인 불가
+		return true;
 	}
 
 	@Override
 	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+		// 비밀번호 만료 여부
+		// true  : 만료 안됨
+		// false : 만료 됨, 로그인 안됨
+		return true;
 	}
 	
-	
+	//isEnabled
+	// 계정 사용 여부
+	// true  : 계정 활성화(계정 사용 가능)
+	// false : 계정 비활성화(계정 사용 불가, 로그인 불가)
 	
 }
